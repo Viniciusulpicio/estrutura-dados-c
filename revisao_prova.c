de9a175cffd48f03779ea5147a4934e665e412a7
Vinicius Sulpicio <viniciusulpicio@gmail.com>
2026-09-20 22:28:21 -0300

    feat: exercicios de revisao

--- revisao_prova.c
+++ revisao_prova.c
@@ -21,11 +21,87 @@ void aula1(){
 }
 
 void aula2(){
-    
+    float n1, n2, media, exame;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &n1);
+    printf("Digite a segunda nota: ");
+    scanf("%f", &n2);
+
+    media = (n1+n2) / 2;
+
+    if(media >= 7){
+        printf("Aprovado");
+    }else if(media <= 3){
+        printf("Reprovado");
+    }else{
+        exame = 10 - media;
+        printf("A nota necessaria para o exame é: %.1f", exame);
+    }
+
+}
+
+void aula3(){
+    int i = 0;
+    for (i = 0; i < 500; i++){
+        if (i % 3 == 0){
+            printf("%d \n", i);
+        }
+
+    }
+}
+
+void aula4(){
+    int i = 0, soma = 0;
+
+    while(i < 100){
+        i++;
+        soma += i;
+    }
+    printf("A soma dos valores é: %d", soma);
+}
+
+void aula6(){
+
 }
 
 void main(){
     setlocale(LC_ALL, "Portuguese");
     //aula1();
-    aula2();
+    //aula2();
+    //aula3();
+    //aula4();
+    aula6();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
